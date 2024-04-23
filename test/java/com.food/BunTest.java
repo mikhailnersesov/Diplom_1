17c379fde4a7b33aa4def9c1abbd2a05b069a8c5
Mikhail Nersesov <mikhail.nersesov@gmail.com>
2024-04-23 15:54:20 +0200

    ingridient class ready; overall coverage 33%

--- test/java/com.food/BunTest.java
+++ test/java/com.food/BunTest.java
@@ -6,10 +6,13 @@ import praktikum.Bun;
 import static org.junit.Assert.assertEquals;
 
 public class BunTest {
+    private static final int BUN_PRICE = 5;
+    private static final String BUN_NAME = "Baguette";
+
     @Test
-    public void getNameIvanShowsOk() {
-        String expectedName = "Ivan";
-        Bun bun = new Bun(expectedName, 5);
+    public void getNameCiabattaShowsOk() {
+        String expectedName = "Ciabatta";
+        Bun bun = new Bun(expectedName, BUN_PRICE);
         String actualName = bun.getName();
         assertEquals(expectedName, actualName);
     }
@@ -17,8 +20,8 @@ public class BunTest {
     @Test
     public void getPriceTenShowsOk() {
         float expectedPrice = 10.0F;
-        Bun bun = new Bun("Igor", expectedPrice);
+        Bun bun = new Bun(BUN_NAME, expectedPrice);
         float actualPrice = bun.getPrice();
-        assertEquals(expectedPrice, actualPrice);
+        assertEquals(expectedPrice, actualPrice,0);
     }
 }
---
+++ test/java/praktikum/IngredientTest.java
@@ -0,0 +1,35 @@
+package praktikum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IngredientTest {
+    private static final IngredientType INGREDIENT_TYPE = null;
+    private static final String INGREDIENT_NAME = "Bread";
+    private static final float INGRDIENT_PRICE = 15.0F;
+
+    @Test
+    public void getPriceTenShowsOk() {
+        float expectedPrice = 10.0F;
+        Ingredient ingredient = new Ingredient(INGREDIENT_TYPE, INGREDIENT_NAME, expectedPrice);
+        float actualPrice = ingredient.getPrice();
+        assertEquals(expectedPrice, actualPrice,0);
+    }
+
+    @Test
+    public void getNameMilkShowsOk() {
+        String expectedName = "Milk";
+        Ingredient ingredient = new Ingredient(INGREDIENT_TYPE, expectedName, INGRDIENT_PRICE);
+        String actualName = ingredient.getName();
+        assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void getType() {
+        IngredientType expectedType = null; //TODO
+        Ingredient ingredient = new Ingredient(expectedType, INGREDIENT_NAME, INGRDIENT_PRICE);
+        IngredientType actualName = ingredient.getType();
+        assertEquals(expectedType, actualName);
+    }
+}
