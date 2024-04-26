40c45f8632a24d65f6befb712117e007416608be
Mikhail Nersesov <mikhail.nersesov@gmail.com>
2024-04-26 23:03:30 +0200

    39% line coverage, ingedientType is covered

--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -1,10 +1,18 @@
 package praktikum;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
 public class IngredientTypeTest {
-    public void testEnumValuesShowsOk(){
+    @Test
+    public void sauceEnumValueShowsOk(){
         assertEquals(IngredientType.SAUCE, IngredientType.valueOf("SAUCE"));
+    }
+    @Test
+    public void fillingEnumValueShowsOk(){
         assertEquals(IngredientType.FILLING, IngredientType.valueOf("FILLING"));
     }
 
