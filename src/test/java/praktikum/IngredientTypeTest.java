cb27d7322c90ef69ced988e8770e1e1b72796e90
Mikhail Nersesov <mikhail.nersesov@gmail.com>
2024-05-12 15:07:19 +0200

    enum test descriptions updated

--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class IngredientTypeTest {
+    // this test ensures, that if enum is deleted - code will not even compile and we will be informed about problems
+    // enum itself are tested when they are used in the tests
     @Test
     public void sauceEnumValueShowsOk() {
         assertEquals(IngredientType.SAUCE, IngredientType.valueOf("SAUCE"));
